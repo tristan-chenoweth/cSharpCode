75dd37a839ce556913d093a1a1e84e00c41964b3
tristan-chenoweth <tristan.chenoweth@outlook.com>
2022-02-28 23:59:36 -0600

    update of file

---
+++ basicPrograms/BugSquasher.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace BugSquasher
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      double number = 38498.3222;
+      string dinosaur = "Barney";
+      double lock1 = 293.000;
+      bool yes = true;
+      string band = ("The Low Anthem");
+    }
+  }
+}
---
+++ basicPrograms/helloWorld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Tristan Chenoweth");
+      Console.WriteLine("This is a change in the program.");
+     }
+  }
+}
