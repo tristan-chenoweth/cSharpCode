461e2db62cdb1bf12df501ccbaf29ab44fa9b315
tristan-chenoweth <tristan.chenoweth@outlook.com>
2022-03-25 01:02:36 -0500

    update

--- modulo/Program.cs
+++ modulo/Program.cs
@@ -7,13 +7,13 @@ namespace ClassTeams
         static void Main(string[] args)
         {
         // Number of students
-
+        int students = 18;
 
         // Number of students in a group
-
+        int groupSize = 3;
 
         // Does groupSize go evenly into students?
-
+        Console.WriteLine(students % groupSize);
 
         }
     }
