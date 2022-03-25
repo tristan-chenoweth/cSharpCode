43e0492ab34474abb1762e9540c036fa9961635e
tristan-chenoweth <tristan.chenoweth@outlook.com>
2022-03-25 02:37:14 -0500

    update

---
+++ stringConcatenation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StoryTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare the variables
+            string beginning = "This is the beginning of the story.\n";
+            string middle = "This is the middle of the story.\n";
+            string end = "This is the end of the story.";
+
+            // Concatenate the string and the variables
+            string story = beginning + middle + end;
+
+            // Print the story to the console 
+            Console.WriteLine(story);
+        }
+    }
+}
